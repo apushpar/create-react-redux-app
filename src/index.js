806d2168bfed9976f9e256023315233cdb6a15b7
Akshay Pushparaja <Akshay.Pushparaja@technicolor.com>
2017-07-12 18:31:06 -0700

    super basic react routing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -6,13 +6,26 @@ import ReduxPromise from 'redux-promise';
 import App from './App';
 import './index.css';
 import reducers from './reducers';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
+class Hello extends Component {
+	render() { return <div>Hello!</div> }
+}
+
+class Goodbye extends Component {
+	render() { return <div>Goodbye!</div> }
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+    	<div>
+	    	<Route path="/hello" component={Hello} />
+	    	<Route path="/goodbye" component={Goodbye} />
+    	</div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
