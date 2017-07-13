5ab4e97e645644cef19ef05c7fd13704aef02fce
Akshay Pushparaja <Akshay.Pushparaja@technicolor.com>
2017-07-12 18:50:24 -0700

    posts_index route created

--- src/index.js
+++ src/index.js
@@ -5,25 +5,18 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 import App from './App';
 import './index.css';
-import reducers from './reducers';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
-class Hello extends Component {
-	render() { return <div>Hello!</div> }
-}
-
-class Goodbye extends Component {
-	render() { return <div>Goodbye!</div> }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     	<div>
-	    	<Route path="/hello" component={Hello} />
-	    	<Route path="/goodbye" component={Goodbye} />
+    		<Route path="/" component={PostsIndex} />
     	</div>
     </BrowserRouter>
   </Provider>,
